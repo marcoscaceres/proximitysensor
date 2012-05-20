c84e37da533d4efbdf7559214354c6a2a11d87c7
Marcos Caceres <marcosscaceres@gmail.com>
2012-05-20 21:57:30 +0100

    Cleanup of code, added license. Fixed bug also in DeviceProximity so correct values are returned from fake sensor only once.

--- UserProximityEvent.js
+++ UserProximityEvent.js
@@ -1,44 +1,35 @@
 /**
-This is an implementation of "Proximity Events":
-http://dvcs.w3.org/hg/dap/raw-file/tip/proximity/Overview.html
+Implementation of "User Proximity"
+http://dvcs.w3.org/hg/dap/raw-file/tip/proximity/userproximity.html
 
 Public Domain Software 
 To the extent possible under law, Marcos Caceres has waived all copyright and 
-related or neighboring rights to DeviceProximityEvent Implementation. 
+related or neighboring rights to DeviceProximityEvent Implementation.
 
-This program implements the following intefaces:
+This program implements the following intefaces: 
 
-[Constructor (DOMString type, optional DeviceProximityEventInit eventInitDict)]
-interface DeviceProximityEvent : Event {
-    readonly attribute double value;
-    readonly attribute double min;
-    readonly attribute double max;
+[Constructor (DOMString type, optional UserProximityEventInit eventInitDict)]
+interface UserProximityEvent : Event {
+    readonly attribute boolean near;
 };
-dictionary DeviceProximityEventInit : EventInit {
-    double value;
-    double min;
-    double max;
+dictionary UserProximityEventInit : EventInit {
+    boolean near;
 };
 **/
-(function implementDeviceProximityEvent(globalObject, sensor) {
-    'use strict';
-    var min, max, value, props, iProtoObj,
-    //interface object + constructor
-    iObj = function DeviceProximityEvent(type, eventInitDict) {
+(function implementUserProximityEvent(globalObject, sensor) {
+    var props,
+        iProtoObj,
+        iObj = function UserProximityEvent(type, eventInitDict) {
             var typeString = String(type),
-                converters = Object.create({}),
+                converters = Object.create(null),
                 dict = {
-                    value: sensor.value || +Infinity,
-                    max: sensor.max || +Infinity,
-                    min: sensor.min || -Infinity,
+                    near: sensor.near(),
                     cancelable: false,
                     bubbles: true
                 };
 
             //ECMAScript to WebIDL converters
-            converters.min = toDouble;
-            converters.max = toDouble;
-            converters.value = toDouble;
+            converters.near = toBool;
             converters.bubbles = toBool;
             converters.cancelable = toBool;
 
@@ -46,7 +37,7 @@ dictionary DeviceProximityEventInit : EventInit {
                 throw new TypeError('Not Enough Arguments');
             }
 
-            //process eventInitDict if it was passed
+            //process eventInitDict if it was passed, overriding 'dict'
             if (eventInitDict) {
                 if (Type(eventInitDict) !== 'object') {
                     throw new TypeError('wrong argument');
@@ -66,52 +57,21 @@ dictionary DeviceProximityEventInit : EventInit {
                 }
             }
 
-            min = dict.min;
-            max = dict.max;
-            value = dict.value;
+            //create the near attribute
+            props = {
+                get: function() {
+                    return dict.near;
+                },
+                enumerable: true,
+                configurable: true
+            };
+            Object.defineProperty(this, 'near', props);
 
             this.prototype = new Event(type, {
                 bubbles: dict.bubbles,
                 cancelable: dict.cancelable
             });
-            //create the min attribute
-            props = {
-                get: function () {
-                    return dict.min
-                },
-                enumerable: true,
-                configurable: true
-            };
-            Object.defineProperty(this, 'min', props);
 
-            //create the max attribute
-            props = {
-                get: function () {
-                    return dict.max
-                },
-                enumerable: true,
-                configurable: true
-            };
-            Object.defineProperty(this, 'max', props);
-
-            //create the value attribute
-            props = {
-                get: function () {
-                    return dict.value
-                },
-                enumerable: true,
-                configurable: true
-            };
-            Object.defineProperty(this, 'value', props);
-
-            //WebIDL ECMAScript to double
-            function toDouble(value) {
-                var x = Number(value);
-                if (isNaN(x) || x === Infinity || x === -Infinity) {
-                    throw new TypeError('Cannot convert to double.');
-                }
-                return x;
-            }
             //WebIDL ECMAScript to WebIDL boolean
             function toBool(value) {
                 return Boolean(value);
@@ -162,26 +122,36 @@ dictionary DeviceProximityEventInit : EventInit {
                 D.configurable = X.configurable;
                 return D;
             }
+
         };
 
-    //Set up prototype for iterface
-    DeviceProximityEvent.prototype = new Event('');
+    //Set up prototype for interface
+    UserProximityEvent.prototype = new Event('');
     props = {
         writable: false,
         enumerable: false,
         configurable: false
     };
-    Object.defineProperty(DeviceProximityEvent, 'prototype', props);
-    iProtoObj = new DeviceProximityEvent();
+    Object.defineProperty(UserProximityEvent, 'prototype', props);
 
     //set up the prototype object's constructor
-    iProtoObj.constructor = iObj;
+    UserProximityEvent.constructor = iObj;
     props = {
         writable: true,
         enumerable: false,
         configurable: true
     };
-    Object.defineProperty(DeviceProximityEvent, 'constructor', props);
+    Object.defineProperty(UserProximityEvent, 'constructor', props);
+    iProtoObj = new UserProximityEvent();
+
+    //Add UserProximityEvent to global object (i.e., to Window)
+    props = {
+        writable: true,
+        enumerable: false,
+        configurable: true,
+        value: iObj
+    };
+    Object.defineProperty(globalObject, 'UserProximityEvent', props);
 
     //Set up prototype for interface
     iObj.prototype = iProtoObj;
@@ -192,29 +162,14 @@ dictionary DeviceProximityEventInit : EventInit {
     };
     Object.defineProperty(iObj, 'prototype', props);
 
-
-
-    //Add DeviceProximityEvent to global object (i.e., to Window)
-    props = {
-        writable: true,
-        enumerable: false,
-        configurable: true,
-        value: iObj
-    };
-    Object.defineProperty(globalObject, 'DeviceProximityEvent', props);
-
-    //Interface Prototype Object
-    function DeviceProximityEvent() {
+    //Inteface Prototype Object
+    function UserProximityEvent() {
 
     }
-})(this,
-//fake proximity sensor
-{
-    get min() {
-        return 0.2;
-    }, get max() {
-        return 5.0;
-    }, get value() {
-        return Math.round(this.max * Math.random());
+})(this, 
+   //fake user proximity sensor
+   {
+    near: function() {
+        return Boolean(Math.round(Math.random()));
     }
 });
